31f83141251876f52e46ef626fd7aacabbeb43d9
Lindsay Forrister <lindsayforrister@gmail.com>
2018-08-23 16:30:13 -0500

    Photo placement & tabbed widget set up

--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,40 @@ $(".test").click(function() {
     alert("TEST!");
 });
 
+/* TABBED WIDGET */
 
+$("#tabs").tabs();
+
+
+
+
+
+
+
+$("#slider").slider({
+    range: true,
+    values: [17, 67]
+});
+
+
+
+
+
+
+
+$("#menu").menu();
+
+
+
+
+
+
+// Hover states on the static widgets
+$("#dialog-link, #icons li").hover(
+    function () {
+        $(this).addClass("ui-state-hover");
+    },
+    function () {
+        $(this).removeClass("ui-state-hover");
+    }
+);
