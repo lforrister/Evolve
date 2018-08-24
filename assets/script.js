aa1203770ce437e5e861d3d19ca0deddc274ebb8
Lindsay Forrister <lindsayforrister@gmail.com>
2018-08-24 12:55:49 -0500

    Carousel

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,11 @@
-console.log("Hello world!");
+console.log("SCRIPT.JS TEST TEST TEST");
 
 
-$(".test").click(function() {
-    alert("TEST!");
-});
+
 
 /* TABBED WIDGET */
 
 $("#tabs").tabs();
-
-
-
-
-
-
 
 $("#slider").slider({
     range: true,
@@ -42,3 +34,33 @@ $("#dialog-link, #icons li").hover(
         $(this).removeClass("ui-state-hover");
     }
 );
+
+
+// Carousel:
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " w3-white";
+}
